c904faf480155b8575d20bcabf8a6dd9de1911ff
Graham Hall <ghall89@me.com>
2021-08-09 20:52:21 +0000

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<div>
+				<p className="promptText">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
