376da0cbab16b853c3cfa424ac66677bafa3ed1d
Graham Hall <ghall89@me.com>
2021-08-13 17:55:31 +0000

    randomized background image from unsplash

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Footer from './components/Footer.jsx';
 function App() {
 	return (
 		<div className="App">
+			<div className="backdrop">
+				<img src="https://source.unsplash.com/random?calm" alt="" />
+			</div>
+			<h1>Mindfulness Prompts</h1>
 			<Prompt />
 			<Footer />
 		</div>
