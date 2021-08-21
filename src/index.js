c449cbbc84cbd6a50922980bb8789640adc571c4
Graham Hall <ghall89@me.com>
2021-08-21 00:53:02 +0000

    pwa

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
