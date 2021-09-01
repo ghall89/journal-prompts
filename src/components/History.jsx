777a3b4663cb886140c113f31cf6ebda0a71c379
Graham Hall <ghall89@me.com>
2021-09-01 18:05:45 +0000

    simple modal transition

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -24,6 +24,7 @@ const History = props => {
 				style={customStyles}
 				ariaHideApp={false}
 				contentLabel="Prompt History"
+				closeTimeoutMS={300}
 			>
 				<div className="modalScroll">
 					<ul>
